2675d388d45a803022dbf6b6401cc0a149f8eb3c
ariful-islam-mahin <mahinyayaa@gmail.com>
2021-01-18 22:55:40 +0600

    updated

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,18 +10,20 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Ariful Islam Mahin',
+  subtitle: "I'm a Front-End Developer.",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Me, Ariful Islam Mahin working as a Front-End developer. I have come in the web development world only for my passion. I love creating and developing websites more than any other works.',
+  paragraphTwo:
+    'I am self-motivated and passionate about web development, I am an enthusiastic and dedicated learner.',
+  paragraphThree: 'My core skill is based on JavaScript and React.',
+  resume: 'https://drive.google.com/file/d/1lia4FwVRkksaizL1zgJo2q850rdSAZqP/view', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,29 +31,33 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Creative Agency',
+    info:
+      'This is a single page full-stack web application. Clients can order services and see their ordered services.',
+    info2:
+      'Clients can add review and added review also show on the website. Admin can add new services, make a new admin & see all services at the dashboard.',
+    url: 'https://creative-agency0.web.app/',
+    repo: 'https://github.com/ariful-islam-mahin/creative-agency-client', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Apartment Hunt',
+    info:
+      'This is a single page full-stack house rental application(Team Project). Users can book apartments and see their booking information at the dashboard.',
+    info2: 'Admin can add new apartments & see all bookings information.',
+    url: 'https://apartment-hunt0.web.app/',
+    repo: 'https://github.com/ariful-islam-mahin/apartment-hunt-client', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Volunteer Network',
+    info:
+      'Volunteer Network is a social volunteering events website. Users can register as a volunteer & check all registered events.',
+    info2: 'Admin can add events or delete registered volunteers.',
+    url: 'https://volunteer-network00.web.app/',
+    repo: 'https://github.com/ariful-islam-mahin/volunteer-network-client', // if no repo, the button will not show up
   },
 ];
 
